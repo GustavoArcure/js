47a3d8385838279dd53c96b4bbc75f19f4bbe4eb
Gustavo Arcure <137923684+GustavoArcure@users.noreply.github.com>
2023-08-03 17:31:41 -0300

    Add files via upload

---
+++ js/cadastro/js/acoes.js
@@ -0,0 +1,16 @@
+let nome = document.getElementById('nome')
+let idade = document.getElementById('idade')
+let cargo = document.getElementById('cargo')
+let salario = document.getElementById('salario')
+0
+
+function salvar () {
+    event.preventDefault()
+    document.getElementById('fnome').innerHTML = nome.value
+    document.getElementById('fidade').innerHTML = idade.value
+    document.getElementById('fsalario').innerHTML = cargo.value
+    document.getElementById('fcargo').innerHTML = salario.value
+
+
+
+}
---
+++ js/javascript/projeto.js
@@ -0,0 +1,6 @@
+
+//Mexendo com o DOM do navegador.
+window.alert('Isso é uma interação JS')
+window.confirm('Entendeu o conceito?')
+window.prompt('Qual é o seu nome?')
+console.log('Qual é o seu nome?')
